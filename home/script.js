158635fc949c5b4612b4ea255f74b42cdc4c37e7
githubfitz <fitzgmina@gmail.com>
2024-03-12 20:11:41 +0800

    2

--- home/script.js
+++ home/script.js
@@ -4,3 +4,17 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+
+let changeIcon = function(icon){
+    icon.classList.toggle('fa-times')
+}
+
+// const navLinkEls = document.querySelectorAll('.nav__link');
+
+// navLinkEls.forEach(navLinkEl => {
+//     navLinkEl.addEventListener('click', () =>{
+//         document.querySelector('.active')?.classList.remove('active');
+//         navLinkEl.classList.add('active');
+//     });
+// });
